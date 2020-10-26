b77399635a75fe4b0edc6b035b454ca197e46149
Gulrez <sidd5312@gmailcom>
2020-10-26 14:47:11 +0530

    wip

---
+++ server/public/precache-manifest.59986ed05789655d291b063a6788b94b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54ec08aa2e020e6bde81dda676874c63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b15991ef9d016cf6e06",
+    "url": "/static/css/main.8a971308.chunk.css"
+  },
+  {
+    "revision": "42c44d11c861c4eedffe",
+    "url": "/static/js/2.7e5d88d9.chunk.js"
+  },
+  {
+    "revision": "1b15991ef9d016cf6e06",
+    "url": "/static/js/main.2cd23a91.chunk.js"
+  },
+  {
+    "revision": "5970db7ce03b39a4fc4d",
+    "url": "/static/js/runtime~main.6cc85612.js"
+  }
+]);
--- server/router.js
+++ server/router.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
-router.get('/', (req, res) => {
-    res.send({ response:'Server is up and running.'}).status(200);
-});
+// router.get('/', (req, res) => {
+//     res.send({ response:'Server is up and running.'}).status(200);
+// });
+
+router.use(express.static('client/build'));
+
+router.get('*', (req,res) => 
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+)
 
 module.exports = router;
